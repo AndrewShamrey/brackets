27602ed9a7dac41370d365becfdecc22f82f655f
Andrew <99andreyshamrey99@gmail.com>
2021-02-19 15:47:15 +0300

    final solution

--- src/index.js
+++ src/index.js
@@ -1,101 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const currentConfig = bracketsConfig;
+    const toOpen = currentConfig.slice().map((item) => item[0]);
+    const toClose = currentConfig.slice().map((item) => item[1]);
+    let strToCheck = str;
+    let arrayFromStr = strToCheck.split("");
+    let opened = [];
 
-  let one = [0];
-  let two = [0];
-  let three = [0];
-  let four = [0];
-  let five = [0];
-  let opnBr = [];
-
-  for (let i = 0; i < str.length; i += 1) {
-      for (let j = 0; j < bracketsConfig.length; j += 1) {
-        if (str[i] === bracketsConfig[j][0]) {
-        
-            switch(j) {
-                case 0:
-                    one[one.length-1] += 1; 
-                    one.push(0);
-                    opnBr.push(str[i]);
-                    break;
-                case 1:
-                    two[two.length-1] += 1;
-                    two.push(0);
-                    opnBr.push(str[i]);
-                    break;
-                case 2:
-                    three[three.length-1] += 1;
-                    three.push(0);
-                    opnBr.push(str[i]);
-                    break;     
-                case 3:
-                    four[four.length-1] += 1;
-                    four.push(0);
-                    opnBr.push(str[i]);
-                    break;
-                case 4:
-                    five[five.length-1] += 1;
-                    five.push(0);
-                    opnBr.push(str[i]);
-                    break;
-            }   
-        }
-
-        if (str[i] === bracketsConfig[j][1]) {
-            switch(j) {
-                case 0: 
-                    if (one.length !== 1 && opnBr[opnBr.length - 1] === bracketsConfig[j][0]) {
-                        one.pop();
-                        opnBr.pop();
-                        one[one.length-1] -= 1;  
-                    } else {
-                        return false;
-                    }
-                    break;
-                case 1:
-                    if (two.length !== 1 && opnBr[opnBr.length - 1] === bracketsConfig[j][0]) {
-                        two.pop();
-                        opnBr.pop();
-                        two[two.length-1] -= 1;
-                    } else {
-                        return false;
-                    }
-                    break;
-                case 2:
-                    if (three.length !== 1 && opnBr[opnBr.length - 1] === bracketsConfig[j][0]) {
-                        three.pop();
-                        opnBr.pop();
-                        three[three.length-1] -= 1;                 
-                    } else {
-                        return false;
-                    }
-                    break;     
-                case 3:
-                    if (four.length !== 1 && opnBr[opnBr.length - 1] === bracketsConfig[j][0]) {
-                        four.pop();
-                        opnBr.pop();
-                        four[four.length-1] -= 1;                 
-                    } else {
-                        return false;
-                    }
-                    break;
-                case 4:
-                    if (five.length !== 1 && opnBr[opnBr.length - 1] === bracketsConfig[j][0]) {
-                        five.pop();
-                        opnBr.pop();
-                        five[five.length-1] -= 1;  
-                    } else {
-                        return false;
-                    }
-                    break;
+    for (let i = 0; i < arrayFromStr.length; i++) {
+        if (toOpen.includes(arrayFromStr[i])) {
+            const index = toOpen.indexOf(arrayFromStr[i]);
+            if (currentConfig[index][0] === currentConfig[index][1]) {
+                if (opened[opened.length - 1] === arrayFromStr[i]) {
+                    opened.pop();
+                } else {
+                    opened.push(arrayFromStr[i]);
+                }
+            } else {
+                opened.push(arrayFromStr[i]);
             }
+        } else if (toClose.includes(arrayFromStr[i])) {
+            const closeIndex = toClose.indexOf(arrayFromStr[i]);
+            const openIndex = toOpen.indexOf(opened[opened.length - 1]);
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+            opened.pop();
+        } else {
+            return "Error";
         }
-      }
-  }
+    }
 
-  if (one.length === 1 && two.length === 1 && three.length === 1 && four.length === 1 && five.length === 1) {
-      return true;
-  } else {
-      return false;
-  }
-}
+    if (opened.length) {
+        return false;
+    }
+    return true;
+};
